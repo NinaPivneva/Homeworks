c1209145d8234d8de9ccad7e8af09fd69a7f5c16
NinaPivneva <okovd@yandex.ru>
2022-12-17 22:27:50 +0300

    First homework done

--- Program.cs
+++ Program.cs
@@ -41,9 +41,23 @@
 // 22 3 9 -> 22
 
 
+// Console.WriteLine("Input first number");
+// int a = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Input second number");
+// int b = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Input third number");
+// int c = Convert.ToInt32(Console.ReadLine());
+
+// int max = a;
+
+// if ( b > max) max = b;
+// if ( c > max) max = c;
+
+// Console.WriteLine($"The biggest integer is {max}");
+// // 
 // изначально взять какое то число за максимальное а потом уже сравнивать
-
-
 
 // // ********************************Задание 3:******************************************
 // Напишите программу, которая на вход принимает число 
@@ -55,7 +69,16 @@
 
 // делится на 2 и остаток от деления равен 0
 
-
+// Console.WriteLine("Введите число");
+// int a = Convert.ToInt32(Console.ReadLine());
+// if ((a % 2) == 0)
+// {
+//    Console.WriteLine($"Это число четное");
+// }
+// else
+// {
+//    Console.WriteLine($"Это число нечетное");
+// }
 
 
 // // ********************************Задание 4:******************************************
@@ -65,4 +88,18 @@
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
 
+
+// Console.WriteLine("Введите число");
+// int N = Convert.ToInt32(Console.ReadLine());
+// int res = 1;      
+// if (res == 1)
+// {
+//    res = res + 1;
+// }
+//    while (res <= N)
+// {
+//    Console.WriteLine(res);
+//    res = res + 2;
+// }
+    
 // схитрить - выводить четные, не проверяя на четность
